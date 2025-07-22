245af2cc71f18c27afcfd636a9f21358d1f38ec2
vincent-eboya613 <vincentkidavi@gmail.com>
2025-07-22 18:21:43 +0300

    Update tailwind.config.js

--- alx-project-0x02/tailwind.config.js
+++ alx-project-0x02/tailwind.config.js
@@ -1,18 +1,8 @@
-import type { Config } from "tailwindcss";
-
-export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-} satisfies Config;
+}
