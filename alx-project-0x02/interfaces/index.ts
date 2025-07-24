43cd2928226645f8cd4045c6ce492221b42983d5
vincent-eboya613 <vincentkidavi@gmail.com>
2025-07-24 15:42:24 +0300

    Update index.ts

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -1,9 +1,42 @@
-import React from 'react'
-
-const index = () => {
-  return (
-    <div>index</div>
-  )
+export interface CardProps {
+  title: string;
+  content: string;
 }
 
-export default index
+export interface PostModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onSave: (post: { title: string; content: string }) => void;
+}
+
+export interface ButtonProps {
+  size: "small" | "medium" | "large";
+  shape: "rounded-sm" | "rounded-md" | "rounded-full";
+  children: React.ReactNode;
+}
+
+export interface PostProps {
+  userId: number;
+  title: string;
+  body: string;
+}
+export interface UserProps {
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+  };
+}
